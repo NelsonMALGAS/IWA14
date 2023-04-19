47f540ee2799cd66d4e6e558534ee533b46a809c
NelsonMALGAS <zmalgas69@gmail.com>
2023-04-19 10:08:44 +0200

    Added comments

--- Challenge1/script.js
+++ Challenge1/script.js
@@ -8,7 +8,7 @@ const emoji='\u{1F60D}'
 /**
  *  function called "logTwice". It takes one parameter, which is named "twice".
  *  When this function is called, it will log the value of the "twice" parameter
- *  to the console twice
+ *  to the console 2 times
  * @param {parameter} twice 
  */
 const logTwice = (twice) => {   
